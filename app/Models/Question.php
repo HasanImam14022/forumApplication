8d72831911edbd446d699b300f3c0563c980eebf
Hasan Imam <you@example.com>
2021-02-08 12:04:55 +0600

    Relationship are defined for user question and reply

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
 
 class Question extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+        
+    }
+
+    public function reply()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTO(Category::class);
+    }
 }
--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Question;
+
 
 class Reply extends Model
 {
     use HasFactory;
+
+    public function question()
+    {
+        return $this->belongsTo(Quection::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTO(User::class);
+    }
+
+    public function like()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
