95eda8bcc92d6e262cb116cf69d9236f073c6626
Hasan Imam <you@example.com>
2021-02-12 21:37:39 +0600

    Crude for category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CategoryController extends Controller
 {
@@ -14,7 +15,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        return Category::latest()->get();
     }
 
     /**
@@ -35,7 +36,13 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //Category::create($request->all());
+        $category = new Category;
+
+        $category->name = $request->name;
+        $category->slug = $request->slug;
+        $category->save();
+        return response('Category created',Response::HTTP_CREATED);
     }
 
     /**
@@ -46,7 +53,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        //
+        return $category;
     }
 
     /**
@@ -69,7 +76,18 @@ class CategoryController extends Controller
      */
     public function update(Request $request, Category $category)
     {
-        //
+        //$category->update($request->all());
+
+       /* $category->name = $request->name;
+        $category->slug = $request->slug;
+        $category->update(); */
+
+        $category->update([
+            'name' => $request->name,
+            'slug' => str_slug($request->name)
+        ]);
+
+        return response('Category updated',Response::HTTP_ACCEPTED);
     }
 
     /**
@@ -80,6 +98,7 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        return response('Category deleted');
     }
 }
